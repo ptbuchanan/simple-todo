94d95da9a87f5790b96d160fef0c90d7d55029ee
ptbuchanan <ptbuchanan@loyola.edu>
2023-08-05 21:32:27 -0400

    To-dos' being displayed and created

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,32 +1,53 @@
+import {useState} from "react"
 import "./App.css"
 
 export default function App() {
+  /* Returning an array of two different values:  
+  First is whatever the value is
+  Second is a function of the new value
+  */
+  const [newItem, setNewItem] = useState("Item 1")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newItem, completed: 
+          false},
+      ]     
+    })
+  }
+   
+
   return (
     <> {/* Wrapping in a fragment since having multiple elements returned */}
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input type="text" id="item"/>
+        <input 
+          value={newItem} 
+          onChange={e => setNewItem(e.target.value)} 
+          type="text" 
+          id="item"/>
     </div>
       <button className="btn">Add</button>
     </form>
     <h1 className="header">Todo List</h1>
-    {/* Unordered list */}
     <ul className="list">
-      <li>
+    {todos.map(todo => {
+      return ( 
+      <li key={todo.id}>  {/* React needs to know which element changed.  The key let's React know.  Do not use the index */}
         <label>
-          <input type="checkbox"/>
-          Item 1
+          <input type="checkbox" checked={todo.completed}/>
+          {todo.title}
         </label>
-        <button className="btn btn-danger"></button>
+        <button className="btn btn-danger">Delete</button>
       </li>
-      <li>
-        <label>
-          <input type="checkbox"/>
-          Item 2
-        </label>
-        <button className="btn btn-danger"></button>
-      </li>
+      )
+    })}
     </ul>
     </>
   )
